76186147c6aa890f0e82b3fb5be63bf1f06e57e8
zzy <1789039600@qq.com>
2018-12-13 19:15:55 +0800

    Default Changelist

---
+++ src/guns/src/main/java/cn/stylefeng/guns/modular/spark/service/IPartTimeService.java
@@ -0,0 +1,16 @@
+package cn.stylefeng.guns.modular.spark.service;
+
+import cn.stylefeng.guns.modular.spark.model.PartTime;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 周志勇
+ * @since 2018-12-13
+ */
+public interface IPartTimeService extends IService<PartTime> {
+
+}
